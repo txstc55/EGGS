39de84c5255d82b30dc67757a95be3f4dd298a59
Xuan Tang <txstc55@gmail.com>
2020-03-25 17:01:19 -0400

    test scripts

---
+++ do_test.py
@@ -0,0 +1,303 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Process demo type and file name')
+parser.add_argument("--d", default= 1, help="Demo type, from 1 to 3")
+parser.add_argument("--f", default= "", help="input mesh name, has to be in tutorial/data folder already")
+parser.add_argument("--e", default= 0, help="Example, 0 for slim, 1 for cotangent smoothing, 2 for optical flow")
+parser.add_argument("--w", default= "1000000", help="Example, 0 for slim, 1 for cotangent smoothing")
+args = parser.parse_args()
+file = args.f
+example = int(args.e)
+demo_type = int(args.d)
+weight = str(int(args.w))
+
+if (file!=""):
+    file = " -f "+file
+
+
+demo_type = " -d "+ str(demo_type) 
+## check if build exists
+if not os.path.isdir("build"):
+    print("Creating build folder now")
+    os.system("mkdir build")
+    os.system("cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j && cd ..")
+else:
+    print("Build exists, rebuilding")
+    os.system("cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j && cd ..")
+
+## check if result data folder exists
+if not os.path.isdir("result_datas"):
+    print("Creating result datas folder now")
+    os.system("mkdir result_datas")
+
+
+eigen_data = {}
+eigen_data["name"] = "EIGEN"
+eigen_data["COMPUTE"] = 0
+eigen_data["SOLVE"] = 0
+eigen_data["ASSEMBLE"] = 0
+
+mkl_data = {}
+mkl_data["name"] = "MKL"
+mkl_data["COMPUTE"] = 0
+mkl_data["SOLVE"] = 0
+mkl_data["ASSEMBLE"] = 0
+
+
+numeric1_data = {}
+numeric1_data["name"] = "NUMERIC1"
+numeric1_data["COMPUTE"] = 0
+numeric1_data["SOLVE"] = 0
+numeric1_data["ASSEMBLE"] = 0
+
+numeric2_data = {}
+numeric2_data["name"] = "NUMERIC2"
+numeric2_data["COMPUTE"] = 0
+numeric2_data["SOLVE"] = 0
+numeric2_data["ASSEMBLE"] = 0
+
+if example == 0:
+    ## do eigen tests
+    os.system("cd build && ./tutorial/709_SLIM_bin -m 0" + demo_type + file +" && cd ..")
+    os.system("mv build/result.txt result_datas/result_eigen.txt")
+
+
+    # ## do cached tests
+    # os.system("cd build && ./tutorial/709_SLIM_bin -m 1" + demo_type + file +" && cd ..")
+    # os.system("mv build/result.txt result_datas/result_cached.txt")
+
+
+
+    ## do mkl tests
+    os.system("cd build && ./tutorial/709_SLIM_bin -m 2" + demo_type + file +" && cd ..")
+    os.system("mv build/result.txt result_datas/result_mkl.txt")
+
+
+    ## do numeric 1 tests
+    os.system("cd build && ./tutorial/709_SLIM_bin -m 3" + demo_type + file +" && cd ..")
+    os.system("mv build/result.txt result_datas/result_numeric1.txt")
+
+    ## do numeric 2 tests
+    os.system("cd build && ./tutorial/709_SLIM_bin -m 4" + demo_type + file +" && cd ..")
+    os.system("mv build/result.txt result_datas/result_numeric2.txt")
+
+
+
+    f = open("result_datas/result_eigen.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%11
+            if count == 0 or count == 1 or count == 2 or count == 3 or count == 5 or count == 6 or count == 8:
+                eigen_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 4 or count == 7:
+                eigen_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                eigen_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+
+    # cached_data = {}
+    # cached_data["name"] = "CACHED"
+    # cached_data["COMPUTE"] = 0
+    # cached_data["SOLVE"] = 0
+    # cached_data["ASSEMBLE"] = 0
+    # f = open("result_datas/result_cached.txt")
+    # count = 0
+    # for line in f:
+    #     if not line.startswith("START"):
+    #         count = count%11
+    #         if count == 0 or count == 1 or count == 2 or count == 5 or count == 6 or count == 8:
+    #             cached_data["ASSEMBLE"] += float(line.split(": ")[1])
+    #         elif count == 3 or count == 4 or count == 7:
+    #             cached_data["COMPUTE"] += float(line.split(": ")[1])
+    #         else:
+    #             cached_data["SOLVE"] += float(line.split(": ")[1])
+    #         count+=1
+
+
+
+    f = open("result_datas/result_mkl.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%14
+            if (count >= 0 and count<=8) or count == 11:
+                mkl_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 9 or count == 10:
+                mkl_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                mkl_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+
+
+    f = open("result_datas/result_numeric1.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%9
+            if (count >= 0 and count<=5):
+                numeric1_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 6:
+                numeric1_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                numeric1_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+
+
+    f = open("result_datas/result_numeric2.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%10
+            if (count >= 0 and count<=3) or count == 5:
+                numeric2_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 4 or count == 6 or count == 7:
+                numeric2_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                numeric2_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+
+
+    import json
+    with open('result_datas/all_result.json', 'w') as j:
+        json.dump([eigen_data,mkl_data,numeric1_data,numeric2_data],j)
+elif example == 1:
+    ## do eigen tests
+    os.system("cd build && ./tutorial/205_Laplacian_bin -w "+weight+" -m 0"+ file +" && cd ..")
+    os.system("mv build/result_cot.txt result_datas/result_cot_eigen.txt && mv build/cot_smoothed.obj result_datas/cot_smoothed_eigen.obj")
+
+    ## do mkl tests
+    os.system("cd build && ./tutorial/205_Laplacian_bin -w "+weight+" -m 2"+ file +" && cd ..")
+    os.system("mv build/result_cot.txt result_datas/result_cot_mkl.txt && mv build/cot_smoothed.obj result_datas/cot_smoothed_mkl.obj")
+
+
+    ## do numeric 1 tests
+    os.system("cd build && ./tutorial/205_Laplacian_bin -w "+weight+" -m 3"+ file +" && cd ..")
+    os.system("mv build/result_cot.txt result_datas/result_cot_numeric1.txt && mv build/cot_smoothed.obj result_datas/cot_smoothed_numeric1.obj")
+
+    ## do numeric 2 tests
+    os.system("cd build && ./tutorial/205_Laplacian_bin -w "+weight+" -m 4"+ file +" && cd ..")
+    os.system("mv build/result_cot.txt result_datas/result_cot_numeric2.txt && mv build/cot_smoothed.obj result_datas/cot_smoothed_numeric2.obj")
+
+
+    f = open("result_datas/result_cot_eigen.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%7
+            if count <=2 or count == 4:
+                eigen_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 3:
+                eigen_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                eigen_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+    f = open("result_datas/result_cot_mkl.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%9
+            if count<=3 or count == 6:
+                mkl_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 4 or count == 5:
+                mkl_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                mkl_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+    f = open("result_datas/result_cot_numeric1.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%6
+            if count<=2:
+                numeric1_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 3:
+                numeric1_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                numeric1_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+
+    f = open("result_datas/result_cot_numeric2.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%7
+            if count<=2:
+                numeric2_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 4 or count == 3:
+                numeric2_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                numeric2_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+
+    import json
+    with open('result_datas/all_result_cot.json', 'w') as j:
+        json.dump([eigen_data,mkl_data,numeric1_data,numeric2_data],j)
+
+elif example==2:
+    ## do eigen tests
+    os.system("cd build && ./tutorial/801_OpticalFlow_bin -m 0" +" && cd ..")
+    os.system("mv build/result_opt.txt result_datas/result_opt_eigen.txt")
+
+
+    ## do numeric multi tests
+    os.system("cd build && ./tutorial/801_OpticalFlow_bin -m 3" +" && cd ..")
+    os.system("mv build/result_opt.txt result_datas/result_opt_numeric1.txt")
+
+    ## do numeric single tests
+    os.system("cd build && ./tutorial/801_OpticalFlow_bin -m 4" +" && cd ..")
+    os.system("mv build/result_opt.txt result_datas/result_opt_numeric2.txt")
+
+
+    f = open("result_datas/result_opt_eigen.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%7
+            if count == 0 or count == 3 or count == 6:
+                eigen_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 1 or count == 2:
+                eigen_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                eigen_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+    f = open("result_datas/result_opt_numeric1.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%6
+            if count == 0 or count == 2 or count == 5:
+                numeric1_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 1:
+                numeric1_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                numeric1_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+
+    f = open("result_datas/result_opt_numeric2.txt")
+    count = 0
+    for line in f:
+        if not line.startswith("START"):
+            count = count%4
+            if count == 3:
+                numeric2_data["ASSEMBLE"] += float(line.split(": ")[1])
+            elif count == 0:
+                numeric2_data["COMPUTE"] += float(line.split(": ")[1])
+            else:
+                numeric2_data["SOLVE"] += float(line.split(": ")[1])
+            count+=1
+
+    import json
+    with open('result_datas/all_result_opt.json', 'w') as j:
+        json.dump([eigen_data,numeric1_data,numeric2_data],j)
---
+++ plot.py
@@ -0,0 +1,50 @@
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+results = []
+if len(sys.argv) == 2:
+    with open(sys.argv[1], 'r') as j:
+        results = json.load(j)
+else:
+    with open('result_datas/all_result.json', 'r') as j:
+        results = json.load(j)
+
+assemble = [float(x["ASSEMBLE"])/100 for x in results]
+compute = [float(x["COMPUTE"])/100 for x in results]
+solve = [float(x["SOLVE"])/100 for x in results]
+
+# total_time =[assemble[i]+compute[i]+solve[i] for i in range(5)]
+
+bars = np.add(solve, compute).tolist()
+
+N = len(results)
+
+ind = np.arange(N)    # the x locations for the groups
+width = 0.35       # the width of the bars: can also be len(x) sequence
+
+p1 = plt.bar(ind, solve, width)
+p2 = plt.bar(ind, compute, width, bottom=solve)
+p3 = plt.bar(ind, assemble, width, bottom=bars)
+
+plt.ylabel('Time')
+plt.title('Method')
+plt.xticks(ind, [x["name"] for x in results])
+plt.yticks(np.arange(0, 81, 10))
+plt.legend((p1[0], p2[0], p3[0]), ('Solve', 'Compute', 'Assemble'))
+
+
+for r1, r2, r3 in zip(p1, p2, p3):
+    h1 = r1.get_height()
+    h2 = r2.get_height()
+    h3 = r3.get_height()
+    plt.text(r1.get_x() + r1.get_width() / 2., h1 / 2., "%d" % h1, ha="center", va="center", color="white", fontsize=5, fontweight="bold")
+    plt.text(r2.get_x() + r2.get_width() / 2., h1 + h2 / 2., "%d" % h2, ha="center", va="center", color="white", fontsize=5, fontweight="bold")
+    plt.text(r3.get_x() + r3.get_width() / 2., h1 + h2 + h3 / 2., "%d" % h3, ha="center", va="center", color="white", fontsize=5, fontweight="bold")
+
+plt.show()
+
+
+print(results)
