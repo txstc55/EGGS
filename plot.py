64927427b9e9879533148698923d2d7fdbd4b7ac
Xuan Tang <txstc55@gmail.com>
2020-03-27 17:06:23 -0400

    better plot legends

--- plot.py
+++ plot.py
@@ -16,6 +16,10 @@ assemble = [float(x["ASSEMBLE"])/100 for x in results]
 compute = [float(x["COMPUTE"])/100 for x in results]
 solve = [float(x["SOLVE"])/100 for x in results]
 
+sum = []
+for i in range(len(assemble)):
+    sum.append(assemble[i]+compute[i]+solve[i])
+
 # total_time =[assemble[i]+compute[i]+solve[i] for i in range(5)]
 
 bars = np.add(solve, compute).tolist()
@@ -29,10 +33,11 @@ p1 = plt.bar(ind, solve, width)
 p2 = plt.bar(ind, compute, width, bottom=solve)
 p3 = plt.bar(ind, assemble, width, bottom=bars)
 
-plt.ylabel('Time')
-plt.title('Method')
+plt.ylabel('Time in Microseconds')
+plt.title('Time Result For Each Method')
+plt.xlabel('Method')
 plt.xticks(ind, [x["name"] for x in results])
-plt.yticks(np.arange(0, 81, 10))
+plt.yticks(np.arange(0, max(sum)*1.1, max(sum)*1.1/11.0))
 plt.legend((p1[0], p2[0], p3[0]), ('Solve', 'Compute', 'Assemble'))
 
 
@@ -40,9 +45,12 @@ for r1, r2, r3 in zip(p1, p2, p3):
     h1 = r1.get_height()
     h2 = r2.get_height()
     h3 = r3.get_height()
-    plt.text(r1.get_x() + r1.get_width() / 2., h1 / 2., "%d" % h1, ha="center", va="center", color="white", fontsize=5, fontweight="bold")
-    plt.text(r2.get_x() + r2.get_width() / 2., h1 + h2 / 2., "%d" % h2, ha="center", va="center", color="white", fontsize=5, fontweight="bold")
-    plt.text(r3.get_x() + r3.get_width() / 2., h1 + h2 + h3 / 2., "%d" % h3, ha="center", va="center", color="white", fontsize=5, fontweight="bold")
+    plt.text(r1.get_x() + r1.get_width() / 2., h1 / 2., "%d" % h1,
+             ha="center", va="center", color="white", fontsize=5, fontweight="bold")
+    plt.text(r2.get_x() + r2.get_width() / 2., h1 + h2 / 2., "%d" % h2,
+             ha="center", va="center", color="white", fontsize=5, fontweight="bold")
+    plt.text(r3.get_x() + r3.get_width() / 2., h1 + h2 + h3 / 2., "%d" %
+             h3, ha="center", va="center", color="white", fontsize=5, fontweight="bold")
 
 plt.show()
 
