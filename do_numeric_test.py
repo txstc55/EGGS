87aa257088a49ec69c8c8ad76aee3fe2fc61c3bc
Xuan Tang <txstc55@gmail.com>
2020-04-09 22:57:01 -0400

    new test scripts

--- do_numeric_test.py
+++ do_numeric_test.py
@@ -3,6 +3,17 @@ import argparse
 import json
 
 matrix_sizes = [100, 1000, 10000, 100000, 1000000]
+numeric_multi_result = {}
+numeric_single_result = {}
+mkl_multi_result = {}
+mkl_single_result = {}
+eigen_single_result = {}
+mkl_single_result["method_name"] = "MKL SINGLE THREAD"
+mkl_multi_result["method_name"] = "MKL MULTI THREAD"
+numeric_single_result["method_name"] = "OURS SINGLE THREAD"
+numeric_multi_result["method_name"] = "OURS MULTI THREAD"
+eigen_single_result["method_name"] = "EIGEN SINGLE THREAD"
+
 
 if not os.path.isdir("build"):
     print("Creating build folder now")
@@ -12,128 +23,117 @@ else:
     print("Build exists, rebuilding")
     os.system("cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j && cd ..")
 
+
 os.system("cd build && rm *_result.txt && cd ..")
-
 for i in matrix_sizes:
-    for j in range(5):
+    for j in range(3):
         print("Matrix size "+str(i))
-        os.system("cd build && ./tutorial/803_Numeric_bin -d " +
+        os.system("cd build && taskset -c 0-3 ./tutorial/803_Numeric_bin -d " +
                   str(j) + " -r "+str(i) + " && cd ..")
-
 os.system("mv build/*_result.txt result_datas/")
 
 
-numeric_multi_result = {}
-numeric_single_result = {}
-mkl_multi_result = {}
-mkl_single_result = {}
+def write_json(input_file, output_file):
+    mkl_single_result["time"] = []
+    mkl_multi_result["time"] = []
+    numeric_single_result["time"] = []
+    numeric_multi_result["time"] = []
+    eigen_single_result["time"] = []
+    numeric_multi_result["pre_comp"] = []
 
-mkl_single_result["method_name"] = "MKL SINGLE THREAD"
-mkl_multi_result["method_name"] = "MKL MULTI THREAD"
-numeric_single_result["method_name"] = "OURS SINGLE THREAD"
-numeric_multi_result["method_name"] = "OURS MULTI THREAD"
+    f = open(input_file, 'r')
+    count = 0
+    for line in f:
+        time = float(line.split(": ")[-1].split()[0])
+        if count % 6 == 0:
+            numeric_multi_result["pre_comp"].append(time)
+        elif count % 6 == 1:
+            mkl_multi_result["time"].append(time)
+        elif count % 6 == 2:
+            mkl_single_result["time"].append(time)
+        elif count % 6 == 3:
+            numeric_multi_result["time"].append(time)
+        elif count % 6 == 4:
+            numeric_single_result["time"].append(time)
+        elif count % 6 == 5:
+            eigen_single_result["time"].append(time)
+        count += 1
 
-mkl_single_result["time"] = []
-mkl_multi_result["time"] = []
-numeric_single_result["time"] = []
-numeric_multi_result["time"] = []
-
-f = open("result_datas/pipe_result.txt", 'r')
-count = 0
-for line in f:
-    time = float(line.split(": ")[-1].split()[0])
-    if count % 4 == 0:
-        mkl_multi_result["time"].append(time)
-    elif count % 4 == 1:
-        mkl_single_result["time"].append(time)
-    elif count % 4 == 2:
-        numeric_multi_result["time"].append(time)
-    elif count % 4 == 3:
-        numeric_single_result["time"].append(time)
-    count += 1
-
-with open("result_datas/pipe.json", 'w')as j:
-    json.dump([mkl_multi_result, mkl_single_result, numeric_multi_result, numeric_single_result], j)
+    with open(output_file, 'w')as j:
+        json.dump([mkl_multi_result, mkl_single_result,
+                   numeric_multi_result, numeric_single_result, eigen_single_result], j)
 
 
-mkl_single_result["time"] = []
-mkl_multi_result["time"] = []
-numeric_single_result["time"] = []
-numeric_multi_result["time"] = []
-f = open("result_datas/const_result.txt", 'r')
-count = 0
-for line in f:
-    time = float(line.split(": ")[-1].split()[0])
-    if count % 4 == 0:
-        mkl_multi_result["time"].append(time)
-    elif count % 4 == 1:
-        mkl_single_result["time"].append(time)
-    elif count % 4 == 2:
-        numeric_multi_result["time"].append(time)
-    elif count % 4 == 3:
-        numeric_single_result["time"].append(time)
-    count += 1
+write_json("result_datas/pipe_result.txt", "result_datas/pipe.json")
+write_json("result_datas/sypr_result.txt", "result_datas/sypr.json")
+write_json("result_datas/syrk_result.txt", "result_datas/syrk.json")
 
-with open("result_datas/const.json", 'w')as j:
-    json.dump([mkl_multi_result, mkl_single_result, numeric_multi_result, numeric_single_result], j)
+# do the test with 15 entries per row
+os.system("cd build && rm *_result.txt && cd ..")
+for i in matrix_sizes:
+    for j in range(3):
+        print("Matrix size "+str(i))
+        os.system("cd build && taskset -c 0-3 ./tutorial/803_Numeric_bin -d " +
+                  str(j) + " -r "+str(i) + " -e 15" + " && cd ..")
+os.system("mv build/*_result.txt result_datas/")
 
-mkl_single_result["time"] = []
-mkl_multi_result["time"] = []
-numeric_single_result["time"] = []
-numeric_multi_result["time"] = []
-f = open("result_datas/sypr_result.txt", 'r')
-count = 0
-for line in f:
-    time = float(line.split(": ")[-1].split()[0])
-    if count % 4 == 0:
-        mkl_multi_result["time"].append(time)
-    elif count % 4 == 1:
-        mkl_single_result["time"].append(time)
-    elif count % 4 == 2:
-        numeric_multi_result["time"].append(time)
-    elif count % 4 == 3:
-        numeric_single_result["time"].append(time)
-    count += 1
+write_json("result_datas/pipe_result.txt", "result_datas/pipe_15.json")
+write_json("result_datas/sypr_result.txt", "result_datas/sypr_15.json")
+write_json("result_datas/syrk_result.txt", "result_datas/syrk_15.json")
 
-with open("result_datas/sypr.json", 'w')as j:
-    json.dump([mkl_multi_result, mkl_single_result, numeric_multi_result, numeric_single_result], j)
+os.system("cd build && rm *_result.txt && cd ..")
+for i in ["0", "0-1", "0-3", "0-7", "0-15"]:
+    os.system("cd build && taskset -c "+i +
+              " ./tutorial/803_Numeric_bin -d 1 -r 500000 -e 15 && cd ..")
+os.system("mv build/*_result.txt result_datas/")
+write_json("result_datas/sypr_result.txt", "result_datas/sypr_thread_diff.json")
 
+# mkl_single_result["time"] = []
+# mkl_multi_result["time"] = []
+# numeric_single_result["time"] = []
+# numeric_multi_result["time"] = []
+# eigen_single_result["time"] = []
+# numeric_multi_result["pre_comp"] = []
+# f = open("result_datas/spmv_result.txt", 'r')
+# count = 0
+# for line in f:
+#     time = float(line.split(": ")[-1].split()[0])
+#     if count % 4 == 0:
+#         numeric_multi_result["pre_comp"].append(time)
+#     elif count % 4 == 1:
+#         mkl_multi_result["time"].append(time)
+#     elif count % 4 == 2:
+#         numeric_multi_result["time"].append(time)
+#     elif count % 4 == 3:
+#         eigen_single_result["time"].append(time)
+#     count += 1
 
-mkl_single_result["time"] = []
-mkl_multi_result["time"] = []
-numeric_single_result["time"] = []
-numeric_multi_result["time"] = []
-f = open("result_datas/syrk_result.txt", 'r')
-count = 0
-for line in f:
-    time = float(line.split(": ")[-1].split()[0])
-    if count % 4 == 0:
-        mkl_multi_result["time"].append(time)
-    elif count % 4 == 1:
-        mkl_single_result["time"].append(time)
-    elif count % 4 == 2:
-        numeric_multi_result["time"].append(time)
-    elif count % 4 == 3:
-        numeric_single_result["time"].append(time)
-    count += 1
-
-with open("result_datas/syrk.json", 'w')as j:
-    json.dump([mkl_multi_result, mkl_single_result, numeric_multi_result, numeric_single_result], j)
+# with open("result_datas/spmv.json", 'w')as j:
+#     json.dump([mkl_multi_result, numeric_multi_result], j)
 
 
-mkl_single_result["time"] = []
-mkl_multi_result["time"] = []
-numeric_single_result["time"] = []
-numeric_multi_result["time"] = []
-f = open("result_datas/spmv_result.txt", 'r')
-count = 0
-for line in f:
-    time = float(line.split(": ")[-1].split()[0])
-    if count % 2 == 0:
-        mkl_multi_result["time"].append(time)
-    elif count % 2 == 1:
-        numeric_multi_result["time"].append(time)
-    count += 1
+# mkl_single_result["time"] = []
+# mkl_multi_result["time"] = []
+# numeric_single_result["time"] = []
+# numeric_multi_result["time"] = []
+# f = open("result_datas/const_result.txt", 'r')
+# count = 0
+# for line in f:
+#     time = float(line.split(": ")[-1].split()[0])
+#     if count % 6 == 0:
+#         numeric_multi_result["pre_comp"].append(time)
+#     elif count % 6 == 1:
+#         mkl_multi_result["time"].append(time)
+#     elif count % 6 == 2:
+#         mkl_single_result["time"].append(time)
+#     elif count % 6 == 3:
+#         numeric_multi_result["time"].append(time)
+#     elif count % 6 == 4:
+#         numeric_single_result["time"].append(time)
+#     elif count % 6 == 5:
+#         eigen_single_result["time"].append(time)
+#     count += 1
 
-with open("result_datas/spmv.json", 'w')as j:
-    json.dump([mkl_multi_result, numeric_multi_result], j)
+# with open("result_datas/const.json", 'w')as j:
+#     json.dump([mkl_multi_result, mkl_single_result,
+#                numeric_multi_result, numeric_single_result, eigen_single_result], j)
--- plot_numeric.py
+++ plot_numeric.py
@@ -2,8 +2,10 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.ticker as ticker
-file_list = ["pipe.json", "const.json", "sypr.json", "syrk.json", "spmv.json"]
-file_list_full = ["result_datas/"+x for x in file_list]
+file_list = ["pipe", "sypr", "syrk"]
+file_list_15 = [x+"_15" for x in file_list]
+file_list += file_list_15
+file_list_full = ["result_datas/"+x+".json" for x in file_list]
 
 x_range = [2, 3, 4, 5, 6]
 x_range = [10**x for x in x_range]
@@ -13,13 +15,20 @@ for k in range(len(file_list_full)):
         results = json.load(j)
         print(results)
         time_result = {}
+        pre_comp_time = []
         for r in results:
             time = r['time']
             time = [float(x) for x in time]
             time_result[r['method_name']] = time
+            if (r['method_name'] == "OURS MULTI THREAD"):
+                pre_comp_time = r["pre_comp"]
+
+        pre_comp_time = [float(x) for x in pre_comp_time]
 
         multi_thread_factor = []
         single_thread_factor = []
+        eigen_single_thread_factor = []
+        pre_comp_time = []
         for i in range(5):
             multi_thread_factor.append(
                 time_result["MKL MULTI THREAD"][i]/time_result["OURS MULTI THREAD"][i])
@@ -29,9 +38,11 @@ for k in range(len(file_list_full)):
             for i in range(5):
                 single_thread_factor.append(
                     time_result["MKL SINGLE THREAD"][i]/time_result["OURS SINGLE THREAD"][i])
+                eigen_single_thread_factor.append(
+                    time_result["EIGEN SINGLE THREAD"][i]/time_result["OURS SINGLE THREAD"][i])
             plt.plot(x_range, single_thread_factor,
                      label="Single Thread Speedup", color="#55E6C1")
-        
+
         plt.legend()
         plt.yticks(list(range(0, 13)))
         # plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
@@ -44,6 +55,35 @@ for k in range(len(file_list_full)):
         ax.spines['right'].set_visible(False)
         save_plot_name = "test_result_graphs/" + \
             (file_list[k].split('.')[0]) + ".pdf"
+        print(save_plot_name)
+        plt.savefig(save_plot_name, bbox_inches='tight',
+                    pad_inches=0, dpi=200)
+        plt.close()
+
+        # plot the eigen difference
+        plt.plot(x_range, eigen_single_thread_factor,
+                 label="Single Thread Speedup Over Eigen", color="#d63031")
+        ax = plt.axes()
+        ax.set_xscale('log')
+        ax.xaxis.set_minor_locator(ticker.FixedLocator([]))
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        save_plot_name = "test_result_graphs/" + \
+            (file_list[k].split('.')[0]) + "_eigen.pdf"
+        print(save_plot_name)
+        plt.savefig(save_plot_name, bbox_inches='tight',
+                    pad_inches=0, dpi=200)
+        plt.close()
+
+        plt.plot(x_range, pre_comp_time,
+                 label="Pre Computation Time", color="#b8e994")
+        ax = plt.axes()
+        ax.set_xscale('log')
+        ax.xaxis.set_minor_locator(ticker.FixedLocator([]))
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        save_plot_name = "test_result_graphs/" + \
+            (file_list[k].split('.')[0]) + "_pre_comp.pdf"
         print(save_plot_name)
         plt.savefig(save_plot_name, bbox_inches='tight',
                     pad_inches=0, dpi=200)
