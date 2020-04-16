9bb5034966212d57a92eeaba7be9546ff91bf41a
Xuan Tang <txstc55@gmail.com>
2020-04-16 13:19:04 -0700

    remove a very small example

--- all_plot.sh
+++ all_plot.sh
@@ -4,7 +4,6 @@ python plot.py result_datas/slim_demo2.json
 python plot.py result_datas/slim_demo3.json
 python plot.py result_datas/cot_smoothing_armadillo.json
 python plot.py result_datas/cot_smoothing_armadillo_170k_triangles.json
-python plot.py result_datas/cot_smoothing_cow.json
 python plot.py result_datas/cot_smoothing_10k_surface_37385.json
 python plot.py result_datas/optical_flow_small_example.json
 python plot.py result_datas/optical_flow_large_example.json
