6b9ae8b891f76550313dbcab3a50c12ad31e5eff
Xuan Tang <txstc55@gmail.com>
2020-06-06 12:41:25 -0400

    remove two tests

--- all_plot.sh
+++ all_plot.sh
@@ -3,12 +3,10 @@ python plot.py result_datas/slim_demo1_400k_triangles.json
 python plot.py result_datas/slim_demo2.json
 python plot.py result_datas/slim_demo3.json
 python plot.py result_datas/cot_smoothing_armadillo.json
-python plot.py result_datas/cot_smoothing_armadillo_170k_triangles.json
 python plot.py result_datas/cot_smoothing_10k_surface_37385.json
 python plot.py result_datas/optical_flow_small_example.json
 python plot.py result_datas/optical_flow_large_example.json
 python plot.py result_datas/cot_matrix_armadillo.json
-python plot.py result_datas/cot_matrix_armadillo_170k.json
 python plot.py result_datas/cot_matrix_37385_sf.json
 
 python plot_numeric.py
