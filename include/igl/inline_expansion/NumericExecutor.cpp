5e67565c8b9ef2997820016d17443e07c9b5e81a
txstc55 <txstc55@gmail.com>
2020-06-09 18:19:31 -0700

    reprot time for code generation and compilation

--- include/igl/inline_expansion/NumericExecutor.cpp
+++ include/igl/inline_expansion/NumericExecutor.cpp
@@ -1,6 +1,7 @@
 #include "NumericExecutor.hpp"
 #include <tbb/parallel_for.h>
-
+#include <iostream>
+#include "../Timer.h"
 using namespace std;
 
 namespace ie
@@ -9,22 +10,28 @@ NumericExecutor::NumericExecutor() {}
 
 NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::RowMajor> &R, size_t gap)
 {
+    igl::Timer t;
+    t.start();
     this->gap = gap;
     this->th = NumericVisitorTreeHashing(R.nonZeros());
     for (int i = 0; i < R.nonZeros(); i++)
     {
         R.valuePtr()[i].accept(this->th, i, true);
     }
-
     // for grouped function
     this->tg = TreeToFileVisitorGroupByFunction(gap);
     this->th.accept(this->tg);
+    std::cout<<"Code generation took: "<<t.getElapsedTimeInMilliSec()<<" ms\n";
+    t.start();
     this->tg.compile_file();
     this->tg.link_functions();
+    std::cout<<"Compiling code took: "<<t.getElapsedTimeInMilliSec()<<" ms\n";
     cout << "Linking function completed\n";
 }
 NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::ColMajor> &R, size_t gap)
 {
+    igl::Timer t;
+    t.start();
     this->gap = gap;
     this->th = NumericVisitorTreeHashing(R.nonZeros());
     for (int i = 0; i < R.nonZeros(); i++)
@@ -35,13 +42,18 @@ NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::Col
     // for grouped function
     this->tg = TreeToFileVisitorGroupByFunction(gap);
     this->th.accept(this->tg);
+    std::cout<<"Code generation took: "<<t.getElapsedTimeInMilliSec()<<" ms\n";
+    t.start();
     this->tg.compile_file();
     this->tg.link_functions();
+    std::cout<<"Compiling code took: "<<t.getElapsedTimeInMilliSec()<<" ms\n";
     cout << "Linking function completed\n";
 }
 
 NumericExecutor::NumericExecutor(Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic> &R, size_t gap)
 {
+    igl::Timer t;
+    t.start();
     int matrix_size = R.rows() * R.cols();
     this->gap = gap;
     this->th = NumericVisitorTreeHashing(matrix_size);
@@ -51,8 +63,11 @@ NumericExecutor::NumericExecutor(Eigen::Matrix<ie::NumericType, Eigen::Dynamic, 
     }
     this->tg = TreeToFileVisitorGroupByFunction(gap);
     this->th.accept(this->tg);
+    std::cout<<"Code generation took: "<<t.getElapsedTimeInMilliSec()<<" ms\n";
+    t.start();
     this->tg.compile_file();
     this->tg.link_functions();
+    std::cout<<"Compiling code took: "<<t.getElapsedTimeInMilliSec()<<" ms\n";
     cout << "Linking function completed\n";
 }
 
